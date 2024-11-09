d780e1ddd3dcab9a8f96fe61f338af1842f1f331
ChifundoKauwa <bsc-com-15-21@unima.ac.mw>
2024-11-09 21:48:14 +0200

    used websocket and room id for one-to-one messaging

--- components/Message.js
+++ components/Message.js
@@ -1,50 +1,55 @@
 'use client'
-import Image from 'next/image';
-import {useState,useEffect} from 'react';
-import io from 'socket.io-client';
-import { IoSend } from 'react-icons/io5'
-const socket =io.connect('http://localhost:3001')
+import {useState,useEffect,useRef} from 'react'
+import {io} from 'socket.io-client'
+import {ioSend} from 'react-icons'
+function Message({roomId,userId}){
+    const [message,setMessage] = useState('')
+    const [messages,setMessages]=useState([])
+    const sockeRef=useRef()
+    const messageInputRef=useRef()
 
-function message(){
-    
-    const[message,setMessage]=useState('')
-    const [receviedMessage,setReceivedMessage]=useState('')
-  
-    const SendMessage=()=>{
-        socket.emit('send text',{message})
-        console.log(message);
-    }
     useEffect(()=>{
-        socket.on('text_received',(data)=>{
-            setReceivedMessage(data.message)
-            console.log(data.message)
-        })
-    },[socket])
+      socketRef.current=io();
+      socketRef.current.emit('room',roomId)
+      socketRef.current.on('get message',(newMessage)=>{
+        setMessages((prevMessages)=>[...prevMessages,newMessage])
+      })
+      return()=>{
+        socketRef.current.emit('leave inbox',roomId)
+        socket.current.disconnect();
+      }
+    },[roomId])
+
+    const handleSendText=()=>{
+      if(message.trim()){
+        const newMessage ={text:message, sender:'you',roomId}
+        socketRef.current.emit('send text',newMessage)
+        setMessages((prevMessages)=>[...prevMessages,newMessage])
+        setMessage('')
+        messageInputRef.current.focus();
+      }
+    }
     return(
-        <section className=' flex flex-col h-16 border rounded-lg bg-white-900 opacity-2 rounded text-2xl sm:text-3xl md:text-4xl flex-col'>
-
-            <div className='flex flex-row'>
-                <Image src="/componets/photos/gee.jpej" width={400} height={100} alt='no photo' className=' rounded-full'/>
-
+      <div className='p-4 max-w-md bg-white rounded-lg shadow-md'>
+        <h2 className='text-xl font-semiblod mb-2'>chat with {userId}</h2>
+        <div className='h-96 overflow-y-auto p-4 bg-gray-400 rounded-lg'>
+          {messages.map((sms,index)=>{
+            <div key={index} className='mb-3'>
+              <p className='text-sm font-medium'>{sms.sender}:</p>
+              <p className='text-gray-400 font-medium'>{sms.text}:</p>
             </div>
-           
+          })}
+          <div className='flex sppace-x-2'>
+            <input
+            ref={messageInputRef}
+            type='text' className='flex-1 p-2 border rounded-lg' placeholder='type message' value={message}
+            onChange={(e)=>setMessage(e.target.value)}/>
+            <button className='bg-blue-400 p-2 rounded-lg' onClick={handleSendText}><ioSend size={40}/></button>
 
-            <div className='flex-1 gap-2 overflow-y-auto'>
-                hhhf
+          </div>
 
-            </div>
-            
-        
-            
-            <div>
-                <input type='text' className='text-black font-semiblod' placeholder='type message...'/>
-        
-            <button onClick={SendMessage} className='p-2'><IoSend size={30}/></button>
-            {receviedMessage}
-            
-            </div>
-
-        </section>
+        </div>
+      </div>
     )
 }
-export default message;
+export default Message;
