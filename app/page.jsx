a44c2e5c9ebef1dc1d6fa19ab1938b44b9ea1339
Lusekero Mwanjoka <lusekero04@gmail.com>
2024-11-13 02:48:46 +0200

    merge to main

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginForm from '../components/LoginForm';
 import SignUpForm from '../components/SignUpForm';
-import Dashboard from '../components/Dashboard';
 
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginForm />} />
         <Route path="/signup" element={<SignUpForm />} />
-        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
